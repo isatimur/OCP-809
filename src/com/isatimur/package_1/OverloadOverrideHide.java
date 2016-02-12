3b8444597a489b4ed9311ab182d78dda4f35db66
Исаченко Тимур Игоревич <tisachenko@at-consulting.ru>
2016-02-13 00:13:17 +0300

    Initial commit

---
+++ src/com/isatimur/package_1/OverloadOverrideHide.java
@@ -0,0 +1,43 @@
+package com.isatimur.package_1;
+
+/**
+ * Created by tisachenko on 11.02.16.
+ */
+public class OverloadOverrideHide {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+//a) valid
+//class Book {
+//    static void print(){}
+//}
+//class CourseBook extends Book{
+//    static void print(){}
+//}
+
+//b) cannot be compiled in CourseBook
+//class Book {
+//    static void print(){}
+//}
+//class CourseBook extends Book{
+//    void print(){}
+//}
+
+//c) cannot be compiled
+//class Book {
+//    void print(){}
+//}
+//class CourseBook extends Book{
+//    static void print(){}
+//}
+
+//d) here it compile and override method inherited from Book class in CourseBook class
+//class Book {
+//    void print(){}
+//}
+//class CourseBook extends Book{
+//    void print(){}
+//}
