3145aa329d22a24401c54b93bb215ac524c5db8b
Isa Timur <isatimur.it@gmail.com>
2016-02-18 00:42:43 +0300

    chapter 2 - Enum

---
+++ src/com/isatimur/package_2/TestEnum.java
@@ -0,0 +1,11 @@
+package com.isatimur.package_2;
+
+/**
+ * Created by Тимакс on 17.02.2016.
+ */
+public class TestEnum {
+    public static void main(String args[]) {
+        System.out.println(Level.BEGINNER.name());
+        System.out.println(Level.BEGINNER);
+    }
+}
---
+++ src/com/isatimur/package_2/UseIceCream.java
@@ -0,0 +1,12 @@
+package com.isatimur.package_2;
+
+/**
+ * Created by Тимакс on 17.02.2016.
+ */
+public class UseIceCream {
+    public static void main(String[] args) {
+        System.out.println(IceCream.VANILLA);
+        System.out.println(IceCream.WALNUT);
+        System.out.println(IceCream.WALNUT);
+    }
+}
