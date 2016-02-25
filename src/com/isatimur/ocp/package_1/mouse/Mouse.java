ddc785afc75312ac943dd38dd94499de3e0e1304
Исаченко Тимур Игоревич <tisachenko@at-consulting.ru>
2016-02-25 15:14:53 +0300

    Updated cases about chapter 2 - Andvantage class design

---
+++ src/com/isatimur/ocp/package_1/mouse/Mouse.java
@@ -0,0 +1,19 @@
+package com.isatimur.ocp.package_1.mouse;
+
+import com.isatimur.ocp.package_1.cat.BigCat;
+
+/**
+ * Created by tisachenko on 22.02.16.
+ */
+public class Mouse {
+    public static void main(String[] args) {
+        BigCat cat = new BigCat("Mouse not scared of cats",false,false,04);
+        System.out.println(cat.name);
+//        System.out.println(cat.hasFur);
+//        System.out.println(cat.hasPaws);
+//        System.out.println(cat.id);
+//        cat.hasFur;
+//        cat.hasPaws;
+//        cat.id
+    }
+}
