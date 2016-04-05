f84a8dd44d6912f4ec6c72e16f0e3b0bca50ea59
Исаченко Тимур Игоревич <tisachenko@at-consulting.ru>
2016-04-05 13:57:36 +0300

    Updated IO chapter with examples

--- src/com/isatimur/ocp/io/Read_nd_Write.java
+++ src/com/isatimur/ocp/io/Read_nd_Write.java
@@ -10,14 +10,21 @@ import java.io.IOException;
 public class Read_nd_Write {
 
     public static void main(String[] args) throws IOException {
-        try (FileInputStream fileInputStream = new FileInputStream("/home/tisachenko/Desktop/photoshop. osnovy_standart.pdf");
-             FileOutputStream fileOutputStream = new FileOutputStream("/home/tisachenko/Desktop/Sample2.pdf");) {
+        try (FileInputStream fileInputStream = new FileInputStream("/home/tisachenko/Desktop/test.txt");
+             FileOutputStream fileOutputStream = new FileOutputStream("/home/tisachenko/Desktop/Sample2.txt");) {
 // THIS IS THE SIMPLESCT MBUT NOT SUFFICIANT EXAMPLE OF READING AND WRITING FILE EXAMPLE
             int data;
 //            while ((data = fileInputStream.read()) != -1) {
 //                fileOutputStream.write(data);
 //            }
 //            fileOutputStream.flush();
+
+//            byte[] buff = new byte[2028];
+//            while ((data = fileInputStream.read(buff)) != -1) {
+
+            // this method will fit all left space with zeros "00"
+//                fileOutputStream.write(buff);
+//            }
 
 
             byte[] buff = new byte[1024];
