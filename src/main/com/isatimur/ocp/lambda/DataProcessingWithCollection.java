9330991525ed14596d73894c5842a2ca671ddcae
Timur Isa <tisachenko@at-consulting.ru>
2016-07-09 19:29:10 +0300

    updated exampleon stream api and lamda chapters

---
+++ src/main/com/isatimur/ocp/lambda/DataProcessingWithCollection.java
@@ -0,0 +1,27 @@
+package com.isatimur.ocp.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Comparator.comparing;
+
+/**
+ * Created by developer on 7/8/16.
+ */
+public class DataProcessingWithCollection {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.forEach(System.out::println);
+        people.removeIf(person -> person.getAge() < 20);
+
+        List<String> names = null;
+        names.replaceAll(name -> name.toUpperCase());
+        names.replaceAll(String::toUpperCase);
+
+        people.sort(
+            comparing(Person::getFirstName).thenComparing(Person::getAge)
+        );
+
+    }
+
+}
