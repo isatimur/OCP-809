d26f279c2def07ae6d8cf3a780676f35db786e42
Timur <isatimur.it@gmail.com>
2016-07-11 08:14:37 +0300

    Flat + spliterator+ IntStream

---
+++ src/main/com/isatimur/ocp/stream_api/IntStreamIntro.java
@@ -0,0 +1,51 @@
+package com.isatimur.ocp.stream_api;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.IntSummaryStatistics;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Isachenko Timur on 10.07.2016.
+ */
+public class IntStreamIntro {
+    public static void main(String[] args) throws IOException {
+        Set<String> shakespearWords =
+                Files.lines(Paths.get("words.shakespeare.txt"))
+                        .map(w -> w.toLowerCase())
+                        .collect(Collectors.toSet());
+
+        Set<String> scrabbleWords =
+                Files.lines(Paths.get("ospd.txt"))
+                        .map(w -> w.toLowerCase())
+                        .collect(Collectors.toSet());
+
+        System.out.println("! Words of Shakespeare : " + shakespearWords.size());
+        System.out.println("! Words of Screbble : " + scrabbleWords.size());
+        final int[] scrabbleEnScore = {
+                //a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p, q,r,s,t,u,v,w,x,y, z
+                1, 3, 3, 2, 1, 5, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10
+        };
+
+        Function<String, Integer> score =
+                word -> word.chars().map(letter -> scrabbleEnScore[letter - 'a']).sum();
+        ToIntFunction<String> intScore =
+                word -> word.chars().map(letter -> scrabbleEnScore[letter - 'a']).sum();
+
+        System.out.println("Score of word hamburg: " + intScore.applyAsInt("hamburg"));
+
+        String word = shakespearWords.stream().filter(w -> scrabbleWords.contains(w)).max(Comparator.comparing(score)).get();
+
+        System.out.println("Best word: " + word);
+
+        IntSummaryStatistics intSummaryStatistics = shakespearWords.stream().parallel().filter(scrabbleWords::contains).mapToInt(intScore).summaryStatistics();
+
+        System.out.println("Stats: " + intSummaryStatistics);
+
+    }
+}
---
+++ src/main/com/isatimur/ocp/stream_api/SpliteratorAPI.java
@@ -0,0 +1,80 @@
+package com.isatimur.ocp.stream_api;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Spliterator;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ * Created by dns2 on 05.07.2016.
+ */
+public class SpliteratorAPI {
+    public static void main(String[] args) {
+        Path filePath = Paths.get("people.txt");
+
+        try (Stream<String> lines = Files.lines(filePath);) {
+
+            Spliterator<String> lineSliterator = lines.spliterator();
+            Spliterator<Person> peopleSpliterator = new PersonSpliterator(lineSliterator);
+
+            Stream<Person> people = StreamSupport.stream(peopleSpliterator, false);
+            people.forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
+class Person {
+    private String name;
+    private int age;
+    private String city;
+
+    public Person() {
+    }
+
+    public Person(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
