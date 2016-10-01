7f459c2d7e17409b986f16fa4b496a2f79805212
Якименко Артём Борисович <tilorn1@mail.ru>
2016-10-01 18:39:35 +0300

    добавил пример работы со стеком - чистый LIFO

---
+++ src/main/java/com/isatimur/ocp/generics_collections/collections/queues/StackExample.java
@@ -0,0 +1,35 @@
+package com.isatimur.ocp.generics_collections.collections.queues;
+
+import java.util.Stack;
+import java.util.EmptyStackException;
+
+class StackExample {
+    static void showpush(Stack st, int a) {
+        st.push(new Integer(a));
+        System.out.println("push(" + a + ")");
+        System.out.println("stack: " + st);
+    }
+
+    static void showpop(Stack st) {
+        System.out.print("pop -> ");
+        Integer a = (Integer) st.pop();
+        System.out.println(a);
+        System.out.println("stack: " + st);
+    }
+
+    public static void main(String args[]) {
+        Stack st = new Stack();
+        System.out.println("stack: " + st);
+        showpush(st, 42);
+        showpush(st, 66);
+        showpush(st, 99);
+        showpop(st);
+        showpop(st);
+        showpop(st);
+        try {
+            showpop(st);
+        } catch (EmptyStackException e) {
+            System.out.println("empty stack");
+        }
+    }
+}
