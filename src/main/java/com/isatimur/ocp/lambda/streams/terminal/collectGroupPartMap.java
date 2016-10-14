577687647d91375c9d4d1c1a026a9a076b632574
Якименко Артём Борисович <tilorn1@mail.ru>
2016-10-14 13:04:30 +0300

    стримы и всё такое

--- src/main/java/com/isatimur/ocp/lambda/streams/terminal/collectGroupPartMap.java
+++ src/main/java/com/isatimur/ocp/lambda/streams/terminal/collectGroupPartMap.java
@@ -1,9 +1,6 @@
 package com.isatimur.ocp.lambda.streams.terminal;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -25,7 +22,7 @@ public class collectGroupPartMap {
 
         Stream<String> ohMySet = Stream.of("lions", "tigers", "bears", "bears", "bears", "bears");
 
-        /*Suppose that we don’t want a Listas the value in the map and prefer a Setinstead. No
+        /*Suppose that we don’t want a List as the value in the map and prefer a Setinstead. No
         problem. There’s another method signature that lets us pass a downstream collector. This is
         a second collector that does something special with the values:*/
 
@@ -39,6 +36,44 @@ public class collectGroupPartMap {
         TreeMap<Integer, Set<String>> map3 = ohMyTreemap.collect(
                 Collectors.groupingBy(String::length, TreeMap::new, Collectors.toSet()));
         System.out.println(map3); // {5=[lions, bears], 6=[tigers]}
+
+
+        /*Partitioning is a special case of grouping. With partitioning, there are only two possible
+        groups—true and false. Partitioning is like splitting a list into two parts.
+        Suppose that we are making a sign to put outside each animal’s exhibit. We have two
+        sizes of signs. One can accommodate names with five or fewer characters. The other is
+        needed for longer names. We can partition the list according to which sign we need:*/
+
+        Stream<String> ohMyPartition = Stream.of("lions", "tigers", "bears", "bears", "brs", "rs");
+        Map<Boolean, List<String>> mapPart = ohMyPartition.collect(
+                Collectors.partitioningBy(s -> s.length() <= 5));
+        System.out.println(mapPart); // {false=[tigers], true=[lions, bears]}
+
+        Stream<String> ohMyStringStream = Stream.of("lions", "tigers", "bears");
+        Map<Boolean, Set<String>> map2 = ohMyStringStream.collect(
+                Collectors.partitioningBy(s -> s.length() <= 7, Collectors.toSet()));
+        System.out.println(map2);// {false=[], true=[lions, tigers, bears]}
+
+        /*For example, we can group by the length of the animal
+        name to see how many of each length we have:*/
+        Stream<String> ohMyStringStreamStringStream = Stream.of("lions", "tigers", "bears", "111bears");
+        Map<Integer, Long> map4 = ohMyStringStreamStringStream.collect(Collectors.groupingBy(
+                String::length, Collectors.counting()));
+        System.out.println(map4); // {5=2, 6=1}
+
+        /*Finally, there is a mapping()collector that lets us go down a level and add another
+        collector. Suppose that we wanted to get the first letter of the first animal alphabetically of
+        each length. Why? Perhaps for random sampling. The examples on this part of the exam
+        are fairly contrived as well. We’d write the following*/
+
+        Stream<String> ohMyStringStream1 = Stream.of("lions", "tigers", "bears", "fears", "rearseeee", "hearees");
+        Map<Integer, Optional<Character>> map5 = ohMyStringStream1.collect(
+                Collectors.groupingBy(
+                        String::length,
+                        Collectors.mapping(s -> s.charAt(0),
+                                Collectors.minBy(Comparator.naturalOrder()))));
+        System.out.println(map5); // {5=Optional[b], 6=Optional[t]}
+
 
     }
 }
